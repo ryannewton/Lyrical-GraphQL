6981d1e016c520021bbc2e646d309f2d7245bbd6
Ryan Newton <newton1988@gmail.com>
2018-12-31 23:57:24 -0600

    Adds front-end to app

--- server/schema/lyric_type.js
+++ server/schema/lyric_type.js
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
 const graphql = require('graphql');
-const {
-  GraphQLObjectType,
-  GraphQLList,
-  GraphQLID,
-  GraphQLInt,
-  GraphQLString
-} = graphql;
+const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLInt, GraphQLString } = graphql;
 const Lyric = mongoose.model('lyric');
 
 const LyricType = new GraphQLObjectType({
-  name:  'LyricType',
+  name: 'LyricType',
   fields: () => ({
     id: { type: GraphQLID },
     likes: { type: GraphQLInt },
@@ -18,14 +12,14 @@ const LyricType = new GraphQLObjectType({
     song: {
       type: require('./song_type'),
       resolve(parentValue) {
-        return Lyric.findById(parentValue).populate('song')
+        return Lyric.findById(parentValue)
+          .populate('song')
           .then(lyric => {
-            console.log(lyric)
-            return lyric.song
+            return lyric.song;
           });
-      }
-    }
-  })
+      },
+    },
+  }),
 });
 
 module.exports = LyricType;
